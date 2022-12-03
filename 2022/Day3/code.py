ac10c71b6baf8d7571a8377b38764fe92b804399
PolRubio <aporcrb23@gmail.com>
2022-12-03 11:57:18 +0100

    names acutally matter

--- 2022/Day3/code.py
+++ 2022/Day3/code.py
@@ -1,9 +1,9 @@
 import numpy as np
 with open("input.txt") as f: raw=f.read()
 
-puntuations = {chr(i): i-ord('a')+1 for i in range(ord('a'), ord('z')+1)}
-puntuations.update({chr(i): i-ord('A')+27 for i in range(ord('A'), ord('Z')+1)})
+priority  = {chr(i): i-ord('a')+1 for i in range(ord('a'), ord('z')+1)}
+priority .update({chr(i): i-ord('A')+27 for i in range(ord('A'), ord('Z')+1)})
 
-points = sum(np.sum([puntuations[n] for n in np.intersect1d(np.array([j for j in i])[:int(len(i)/2)], np.array([j for j in i])[int(len(i)/2):])]) for i in raw.splitlines())
+priorities = sum(np.sum([priority[n] for n in np.intersect1d(np.array([j for j in i])[:int(len(i)/2)], np.array([j for j in i])[int(len(i)/2):])]) for i in raw.splitlines())
 
-print(points)
+print(priorities)
