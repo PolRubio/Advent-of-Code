dc863b265147edeb9f5461d3c9a21a95e6f54237
PolRubio <aporcrb23@gmail.com>
2022-12-03 11:47:38 +0100

    finshed

--- 2022/Day3/code.py
+++ 2022/Day3/code.py
@@ -1,10 +1,9 @@
-with open("input2.txt") as f: raw=f.read()
+import numpy as np
+with open("input.txt") as f: raw=f.read()
 
 puntuations = {chr(i): i-ord('a')+1 for i in range(ord('a'), ord('z')+1)}
 puntuations.update({chr(i): i-ord('A')+27 for i in range(ord('A'), ord('Z')+1)})
 
+points = sum(np.sum([puntuations[n] for n in np.intersect1d(np.array([j for j in i])[:int(len(i)/2)], np.array([j for j in i])[int(len(i)/2):])]) for i in raw.splitlines())
 
-
-# points = sum([puntuations[opponent.get(i[0])][you.get(i[-1])] for i in raw.splitlines()])
-
-# print(points)
+print(points)
